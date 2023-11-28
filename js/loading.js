7f815dec06c3a9e75f63ff810bbb2742355200f1
Hector2523 <67517312+Hector2523@users.noreply.github.com>
2023-11-28 17:21:49 -0300

    sobremesas

--- js/loading.js
+++ js/loading.js
@@ -1,7 +1,7 @@
 var time = document.querySelector(".time");
 var load = document.getElementById("load");
 
-setTimeout(loading, 3500);
+setTimeout(loading, 0);
 
 function loading() {
     load.style.opacity = "0";
